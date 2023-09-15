f155b83a3c2d836fffd19f3ce83fdf73da5e1b02
jalvergara <141336057+jalvergara@users.noreply.github.com>
2023-09-15 15:16:17 -0500

    Add files via upload

---
+++ api_dag_etl.py
@@ -0,0 +1,40 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+from etl import extract, transform, load
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 9, 13),  # Update the start date to today or an appropriate date
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+with DAG(
+    'api_dag',
+    default_args=default_args,
+    description='Our first DAG with ETL process!',
+    schedule_interval='@daily',  # Set the schedule interval as per your requirements
+) as dag:
+
+    extract_task = PythonOperator(
+        task_id='extract_task',
+        python_callable=extract,
+    )
+
+    transform_task = PythonOperator(
+        task_id='transform_task',
+        python_callable=transform,
+        )
+
+    load_task = PythonOperator(
+        task_id='load_task',
+        python_callable=load,
+        )
+
+    extract_task >> transform_task >> load_task
